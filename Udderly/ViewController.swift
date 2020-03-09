c9e2d618b89c9a41e957c64f877bcac430d59ca9
William Chen <william.tw.chen@gmail.com>
2020-03-09 19:53:14 +1000

    Initial Commit

---
+++ Udderly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udderly
+//
+//  Created by William Chen on 9/3/20.
+//  Copyright © 2020 Wax Comical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UdderlyTests/UdderlyTests.swift
@@ -0,0 +1,34 @@
+//
+//  UdderlyTests.swift
+//  UdderlyTests
+//
+//  Created by William Chen on 9/3/20.
+//  Copyright © 2020 Wax Comical. All rights reserved.
+//
+
+import XCTest
+@testable import Udderly
+
+class UdderlyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
